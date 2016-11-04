a2e5d9074fd87474d14fd1d95678bf65287f7e85
marcinsztajn <puchlikmarcin@gmail.com>
2016-11-04 10:06:54 +0100

    dodanie bibliotek (statyczna i dynamiczna)

---
+++ moja_biblioteka.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "moja_biblioteka.h"
+
+
+float odejmowanie (float a, float b){
+return(a-b);
+}
+
+float dzielenie( float a, float b){
+return (a/b);
+}
+
+float dodawanie( float a, float b){
+return (a+b);
+}
+
+float mnozenie( float a, float b){
+return (a*b);
+}
+
+
+
+
+
+
---
+++ moja_biblioteka.h
@@ -0,0 +1,12 @@
+ #ifndef MOJA_BIBLIOTEKA_H
+ #define MOJA_BIBLIOTEKA_H
+ #include <stdio.h>
+ /* tutaj są wpisane deklaracje funkcji */
+float odejmowanie (float a, float b);
+float dzielenie( float a, float b);
+float mnozenie( float a, float b);
+float dodawanie( float a, float b);
+
+
+
+ #endif
